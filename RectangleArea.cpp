c03fe3e7c40678b6602ace1e3b7a15dd21ccb66a
debsanm <debsanm@laccd.edu>
2025-08-27 10:15:44 -0700

    Create RectangleArea.cpp

---
+++ RectangleArea.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	// print name of program
+	cout << "Rectangle Calculator" << endl << endl;
+
+	// get height and width from user
+	double height;
+	double width;
+	cout << "Enter height and width: ";
+/* write your code here */
+	// calculate area
+	
+
+	// write output to console
+	
+
+	// return value that indicates normal program exit
+    return 0;
+}
+
